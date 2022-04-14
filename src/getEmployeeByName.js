154edcebf18599907d1a515555dfae8bb194a56b
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-04-14 20:14:00 -0300

    inicia requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+  if (employeeName === undefined) {
+    return {};
+  };
+  const teste = species.filter((element) => {
+    if(element.firstName === employeeName || element.lastName === employeeName) {
+      return element;
+    };
+  )};
+};
 
 module.exports = getEmployeeByName;
