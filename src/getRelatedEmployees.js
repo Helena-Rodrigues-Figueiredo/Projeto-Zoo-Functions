293115dcc3a8adf45b2d1f441d9439de97b3d0ee
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-04-16 19:15:38 -0300

    inicia requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  let resultado;
+  employees.filter((empregado) => {
+    empregado.managers.find((nome) => {
+      if (nome !== id) {
+        resultado = true;
+      }
+      return resultado;
+    });
+    return resultado;
+  });
+  return resultado;
 }
 
 function getRelatedEmployees(managerId) {
