f2c17bc2d3e10d5ca6c61fd9a849ed159653db4f
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-04-16 15:50:30 -0300

    finaliza requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,15 +1,21 @@
-const data = require("../data/zoo_data");
+const data = require('../data/zoo_data');
 
 const { species } = data;
 
 function getSpeciesByIds(...ids) {
-  const encontraEspecie = species.filter((especie) =>
-    ids.find((item) => {
-      if (ids === []) return [];
-      if (item === especie.id) return especie;
-    }),
-  );
-  return encontraEspecie;
+  if (ids.length === 0) {
+    return [];
+  }
+  const especiesSolicitadas = species.filter((element) =>
+    ids.find((value) => {
+      if (element.id === value) {
+        return true;
+      }
+      return false;
+    }));
+  return especiesSolicitadas;
 }
+
+console.log(getSpeciesByIds('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 module.exports = getSpeciesByIds;
