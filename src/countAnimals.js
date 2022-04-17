60f33eebfeb2709ab381df6bd993b61c40f046fc
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-04-17 16:59:36 -0300

    inicia questão 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,6 +1,25 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const todasEspecies = species.map((especie) => especie.name);
+const qntPorEspecie = species.map((especie) => especie.residents.length);
+
 function countAnimals(animal) {
+  const objeto = {};
+  if (animal === undefined) {
+    todasEspecies.forEach((each, i) => {
+      objeto[each] = qntPorEspecie[i];
+    });
+    return objeto;
+  }
+  species.find((cadaEspecie) => {
+    if (animal.specie === cadaEspecie.name) {
+      return cadaEspecie.residents.length;
+    }
+    return cadaEspecie.residents.length;
+  });
 }
+
+console.log(countAnimals({ specie: 'penguins' }));
 
 module.exports = countAnimals;
