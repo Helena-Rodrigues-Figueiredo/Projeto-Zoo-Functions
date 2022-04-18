a3ab686c093b7d073c8de254b371b1edbec8b7b9
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-04-18 19:14:56 -0300

    finaliza questão 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -3,21 +3,18 @@ const data = require('../data/zoo_data');
 const { employees } = data;
 
 function isManager(id) {
-  let resultado;
-  employees.filter((empregado) => {
-    empregado.managers.find((nome) => {
-      if (nome !== id) {
-        resultado = true;
-      }
-      return resultado;
-    });
-    return resultado;
-  });
-  return resultado;
+  return employees.some((gerente) => gerente.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const funcionarios = employees.filter((element) => element.managers.includes(managerId));
+  const lista = funcionarios.map((nome) => `${nome.firstName} ${nome.lastName}`);
+  return lista;
 }
+
+console.log(isManager('5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = { isManager, getRelatedEmployees };
