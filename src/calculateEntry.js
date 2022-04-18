5aeedc4b6e97fd06c67663a79d151669b7d7f700
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-04-18 15:32:20 -0300

    finaliza requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,31 @@
+// const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const obj = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((dado) => {
+    if (dado.age < 18) {
+      obj.child += 1;
+    } else if (dado.age >= 18 && dado.age < 50) {
+      obj.adult += 1;
+    } else {
+      obj.senior += 1;
+    }
+  });
+  return obj;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const crianca = countEntrants(entrants).child * prices.child;
+  const adulto = countEntrants(entrants).adult * prices.adult;
+  const idoso = countEntrants(entrants).senior * prices.senior;
+  const soma = crianca + adulto + idoso;
+  return soma;
 }
 
 module.exports = { calculateEntry, countEntrants };
