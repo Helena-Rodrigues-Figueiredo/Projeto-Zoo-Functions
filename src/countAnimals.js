09efecce79d8c8a9e438acc9efbf1a27544fe2a2
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-04-18 19:32:09 -0300

    finaliza questão 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,32 +1,29 @@
 const data = require('../data/zoo_data');
 
-// const { species } = data;
-// const todasEspecies = species.map((especie) => especie.name);
-// const qntPorEspecie = species.map((especie) => especie.residents.length);
+const { species } = data;
+const todasEspecies = species.map((especie) => especie.name);
+const qntPorEspecie = species.map((especie) => especie.residents.length);
 
 function countAnimals(animal) {
+  const objeto = {};
+  if (!animal) {
+    todasEspecies.forEach((each, i) => {
+      objeto[each] = qntPorEspecie[i];
+    });
+    return objeto;
+  }
+  if (animal.sex === 'female') {
+    const especies = species.find((cadaEspecie) => animal.specie === cadaEspecie.name).residents;
+    return especies.filter((each) => each.sex === 'female').length;
+  }
+  if (animal.sex === 'male') {
+    const especies = species.find((cadaEspecie) => animal.specie === cadaEspecie.name).residents;
+    return especies.filter((each) => each.sex === 'male').length;
+  }
+  const especies = species.find((cadaEspecie) => animal.specie === cadaEspecie.name)
+    .residents.length;
+  return especies;
 }
-// const objeto = {};
-// if (!animal) {
-//   todasEspecies.forEach((each, i) => {
-//     objeto[each] = qntPorEspecie[i];
-//   });
-//   return objeto;
-// } else if (animal.sex === "female") {
-//   const especies = species.find(
-//     (cadaEspecie) => animal.specie === cadaEspecie.name
-//   ).residents;
-//   return especies.filter((each) => each.sex === "female").length;
-// } else if (animal.sex === "male") {
-//   const especies = species.find(
-//     (cadaEspecie) => animal.specie === cadaEspecie.name
-//   ).residents;
-//   return especies.filter((each) => each.sex === "male").length;
-// }
-//   const especies = species.find(
-//     (cadaEspecie) => animal.specie === cadaEspecie.name
-//   ).residents.length;
-//   return especies;
 
 console.log(countAnimals({ specie: 'lions' }));
 
