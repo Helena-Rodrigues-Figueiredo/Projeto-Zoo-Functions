055d589a625aad9c67fb2e9e09e4bba120ba8582
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-04-20 14:13:52 -0300

    refatora requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -8,11 +8,7 @@ function getOldestFromFirstSpecies(id) {
   const animal = func.responsibleFor[0];
   const infAnimal = species.find((dadosAnimal) => dadosAnimal.id === animal).residents;
   const maisVelho = infAnimal.sort((a, b) => b.age - a.age)[0];
-  const nome = maisVelho.name;
-  const sexo = maisVelho.sex;
-  const idade = maisVelho.age;
-  const array = [nome, sexo, idade];
-  return array;
+  return [maisVelho.name, maisVelho.sex, maisVelho.age];
 }
 
 console.log(getOldestFromFirstSpecies('c1f50212-35a6-4ecd-8223-f835538526c2'));
