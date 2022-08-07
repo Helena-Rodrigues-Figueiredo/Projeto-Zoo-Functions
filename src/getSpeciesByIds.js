6457deac13da1185ff4257fa8527681d72a5acc8
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-08-07 15:16:03 -0300

    Refatora requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,19 +3,9 @@ const data = require('../data/zoo_data');
 const { species } = data;
 
 function getSpeciesByIds(...ids) {
-  if (ids.length === 0) {
-    return [];
-  }
-  const especiesSolicitadas = species.filter((element) =>
-    ids.find((value) => {
-      if (element.id === value) {
-        return true;
-      }
-      return false;
-    }));
-  return especiesSolicitadas;
+  if (ids.length === 0) return [];
+  const filter = species.filter((specie) => ids.some((item) => item === specie.id));
+  return filter;
 }
-
-console.log(getSpeciesByIds('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 module.exports = getSpeciesByIds;
