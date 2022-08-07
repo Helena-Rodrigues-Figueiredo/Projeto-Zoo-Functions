10d562ccd7ae28dfdc154eb7f797c6de34f03128
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-08-07 18:38:52 -0300

    Inicia requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options || (Object.keys(options).length <= 2 && (options.sex || options.sorted))) {
+    return species.reduce((acc, animal) => {
+      acc[animal.location].push(animal.name);
+      return acc;
+    }, { NE: [], NW: [], SE: [], SW: [] });
+  }
 }
 
 module.exports = getAnimalMap;
