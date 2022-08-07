a36ce8e2417a297a3c4e245f23a038983894a447
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-08-07 15:42:00 -0300

    Refatora requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -3,14 +3,8 @@ const data = require('../data/zoo_data');
 const { employees } = data;
 
 function getEmployeeByName(employeeName) {
-  let retorno = {};
-  employees.filter((empregado) => {
-    if (employeeName === empregado.firstName || employeeName === empregado.lastName) {
-      retorno = empregado;
-    }
-    return retorno;
-  });
-  return retorno;
+  return employeeName ? employees.find((employee) => employee.firstName === employeeName
+  || employee.lastName === employeeName) : {};
 }
 
 module.exports = getEmployeeByName;
